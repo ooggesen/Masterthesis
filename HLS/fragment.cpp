79ee827048627d0a8fef41ef03996837377a3258
Ole Oggesen <o.oggesen@gmail.com>
2019-07-10 06:20:11 +0200

    Small changes to fragment to fit synthesis warnings.

--- HLS/fragment.cpp
+++ HLS/fragment.cpp
@@ -31,9 +31,15 @@ static void write_out(hls::stream< ap_uint< 8 > > &buffer,
 		hls::stream< bc_packet > &meta,
 		hls::stream< c_data_t > &data){
 	//write data
-	stream_to_big_chunk: for (int i = 0 ; i < hls::ceil((double) size.to_long()*8 / W_DATA) ; i++){
+	c_data_t_to_stream: for (int i = 0 ; i < BC_STREAM_SIZE ; i++){
+#pragma HLS LOOP_FLATTEN off
+
+		if (i >= hls::ceil((double) size.to_long()*8 / W_DATA))
+			break;
+
 		c_data_t c_buffer;
-		for (int j = 0 ; j < W_DATA/8 ; j++){
+		byte_to_c_data_t: for (int j = 0 ; j < W_DATA/8 ; j++){
+#pragma HLS LOOP_FLATTEN off
 			if (size.to_long() > i*W_DATA/8 + j)
 				c_buffer.range(7 + 8*j, 8*j) = buffer.read();
 			else
@@ -51,25 +57,39 @@ static void write_out(hls::stream< ap_uint< 8 > > &buffer,
 	meta.write(bcp);
 }
 
+static void segment_bc_packet(hls::stream< ap_uint< 8 > > &in,
+		bool end,
+		hls::stream< ap_uint< 8 > > &buffer,
+		c_size_t &size,
+		unsigned rabintab[],
+		unsigned rabinwintab[]){
+#pragma HLS PIPELINE off
+
+	c_size_t bc_size;
+	fill_buffer(in, end, bc_size, buffer);
+
+	//segment the input data
+	c_size_t sc_size;
+	rabinseg_in_stream(in, end, buffer, sc_size, rabintab, rabinwintab);
+
+	size = bc_size + sc_size;
+}
+
 void fragment(hls::stream< ap_uint< 8 > > &in, bool end,  hls::stream< bc_packet > &meta, hls::stream< c_data_t > &data ){
+	hls::stream< ap_uint< 8 > , BC_STREAM_SIZE * W_DATA/8 > buffer("buffer"); //contains big chunk data
 	//intialize the rabin lookup tables
 	unsigned rabintab[256], rabinwintab[256];
 	rabininit(rabintab, rabinwintab);
 
 	//run segmentation until end
 	unsigned  l1 = 0;
-	hls::stream< ap_uint< 8 > , BC_STREAM_SIZE * W_DATA/8 > buffer("buffer"); //contains big chunk data
 	fragment_stream: while(!end || !in.empty()){
-#pragma HLS PIPELINE
+#pragma HLS LOOP_FLATTEN off
 		//fill the buffer with a minimum of data for a big chunk packet
-		c_size_t bc_size;
-		fill_buffer(in, end, bc_size, buffer);
-
-		//segment the input data
-		c_size_t sc_size;
-		rabinseg_in_stream(in, end, buffer, sc_size, rabintab, rabinwintab);
+		c_size_t size;
+		segment_bc_packet(in, end, buffer, size, rabintab, rabinwintab);
 
 		//convert to big chunk packet
-		write_out(buffer, l1, sc_size+bc_size, meta, data);
+		write_out(buffer, l1, size, meta, data);
 	}
 }
