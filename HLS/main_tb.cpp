bfc00f36a0c7ddf2ed7b14f1f16a7863f1bbf27c
Ole Oggesen <o.oggesen@gmail.com>
2020-06-16 17:28:00 +0200

    Changed main testbench to serially testing all testbenches.

--- HLS/main_tb.cpp
+++ HLS/main_tb.cpp
@@ -2,22 +2,35 @@
  * This file contains the main function for calling different tb
  *
  * Usage:
- * 	uncomment the corresponding testbench and comment all others.
+ * 	uncomment the testbenches you want to run,
+ * 	but only uncomment one testbench under the category top testbenches.
  */
 
 #include "test_bench.hpp"
 
 int main(){
-	int retval;
+	int retval = 0;
 
-	//retval = bram_tb();
-	//retval = copy_tb();
-	//retval = dedup_tb();
-	//retval = reorder_tb();
-	//retval = reorder_buffer_tb();
-	//retval = rabin_tb();
-	//retval = fragment_refine_tb();
-	retval = fragment_tb();
+	//INDIVIDUAL TESTBENCHES
+//	retval += bram_tb();
+//	std::cout << std::flush;
+//	retval += copy_tb();
+//	std::cout << std::flush;
+//	retval += dedup_tb();
+//	std::cout << std::flush;
+//	retval += reorder_tb();
+//	std::cout << std::flush;
+//	retval += reorder_buffer_tb();
+//	std::cout << std::flush;
+//	retval += rabin_tb();
+//	std::cout << std::flush;
+//	retval += fragment_refine_tb();
+//	std::cout << std::flush;
+//	retval += fragment_tb();
+//	std::cout << std::flush;
+
+	//TOP_TESTBENCHES
+	retval += top_refine_tb();
 
 
 	return retval;
