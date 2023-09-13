5dc5e35ed2181750a76e7494d23422cc4c58f372
Ole Oggesen <o.oggesen@gmail.com>
2023-09-13 14:13:38 +0200

    Added multiple cosim runs which explore whole test space to testDedup.tcl

--- HLS/vitisHLS-implementation/testDedup.tcl
+++ HLS/vitisHLS-implementation/testDedup.tcl
@@ -5,6 +5,15 @@ set work_dir "/mnt/ole/Documents/coderepo/HLS/vitisHLS-implementation/Dedup/solu
 #path to folder for storing cosim reports
 set result_dir "/mnt/ole/Documents/coderepo/HLS/vitisHLS-implementation/reports"	
 
+#get time and date
+set system_time [clock seconds]
+set system_time [clock format $system_time -format %D||%H:%M:%S]
+
+#set different data type keywords to test on
+set data_type(0) duplicate
+set data_type(1) semi
+set data_type(2) unique
+
 open_project Dedup
 set_top top
 
@@ -17,10 +26,24 @@ add_files "src/fragment.cpp src/fragment_refine.cpp src/fragment.hpp src/fragmen
 
 source "./Dedup/solution1/directives.tcl"
 
-for {set i 1000} {$i < 10000} {set i [expr {$i + 1000}]} {
-	exec mkdir -v $result_dir/$i
-    cosim_design -argv -s$i -d"unique"
-    exec mv -v $work_dir/sim/report/ $result_dir/$i
+#synthesize
+csynth_design
+
+#run cosims for data generation
+exec mkdir -v $result_dir/$system_time
+
+for {set dt_it 0} {$dt_it < array size data_type} {incr dt_it}
+
+	exec mkdir -v $result_dir/$system_time/$data_type($dt_it)
+	
+	for {set i 1000} {$i < 1000000} {set i [expr {$i + 2000}]} {
+	
+		exec mkdir -v $result_dir/$system_time/$data_type($dt_it)/$i
+		#cosim
+		cosim_design -argv -s $i -d $data_type($dt_it)
+		#move reports to report folder
+		exec mv -v $work_dir/sim/report/ $result_dir/$system_time/$data_type($dt_it)/$i
+	}
 }
 
 exit
