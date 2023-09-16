4b85cb009e8d9f83ca35d9e29b56702c9ac1c02c
Ole Oggesen <o.oggesen@gmail.com>
2023-09-16 12:32:13 +0200

    Added all add_files commands to testDedup.tcl

--- HLS/vitisHLS-implementation/testDedup.tcl
+++ HLS/vitisHLS-implementation/testDedup.tcl
@@ -31,12 +31,49 @@ set_part {xczu9eg-ffvb1156-2-e}
 create_clock -period 10 -name default
 
 #Files that need include the parsec rabin algorithm
-add_files "src/fragment.cpp src/fragment_refine.cpp src/fragment.hpp src/fragment_refine.hpp src/top.cpp src/top.hpp" -cflags "-I../../../..src"
+add_files "src/fragment.cpp src/fragment_refine.cpp src/fragment.hpp src/fragment_refine.hpp src/top.cpp src/top.hpp" -cflags "-I../../../../src"
+
+#source files
+add_files src/bram.cpp
+add_files src/bram.hpp
+add_files src/bus_def.cpp
+add_files src/bus_def.hpp
+add_files src/dedup.cpp
+add_files src/dedup.hpp
+add_files src/parsec.hpp
+add_files src/parsec/rabin.cpp
+add_files src/parsec/rabin.hpp
+add_files src/reorder.cpp
+add_files src/reorder.hpp
+add_files src/reorder_buffer.cpp
+add_files src/reorder_buffer.hpp
+add_files src/scheduler.cpp
+add_files src/scheduler.hpp
+add_files src/sha1.hpp
+
+#testbenches
+add_files -tb src/Testbenches/bram_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/bus_def_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/dedup_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/fragment_refine_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/fragment_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/main_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/reorder_buffer_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/reorder_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/scheduler_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/test_bench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/test_bench.hpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/test_bench_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/Testbenches/top_tb.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+
 
 source "./Dedup/solution1/directives.tcl"
 
+#simulate the top file
+csim_design -clean
+
 #synthesize the top file
-#csynth_design
+csynth_design
 
 #run cosims for data generation
 set result_dir [file join $result_dir $system_time]
